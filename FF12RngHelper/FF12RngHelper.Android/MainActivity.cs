26598c0b155df8d03024be9c1f61b22ddd8e2ada
Dror Cohen <gangu1307@gmail.com>
2019-09-13 18:13:53 +0300

    Add app icon

--- FF12RngHelper/FF12RngHelper.Android/MainActivity.cs
+++ FF12RngHelper/FF12RngHelper.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace FF12RngHelper.Droid
 {
-    [Activity(Label = "FF12RngHelper", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FF12RngHelper", Icon = "@drawable/Penelo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
